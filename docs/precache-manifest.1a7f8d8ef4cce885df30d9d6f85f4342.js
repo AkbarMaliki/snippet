22f20ca379ad2ed5a19d1eab749b908953cb590c
Maliki <39995350+AkbarMaliki@users.noreply.github.com>
2020-08-30 15:18:38 +0800

    seco

---
+++ docs/precache-manifest.1a7f8d8ef4cce885df30d9d6f85f4342.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6bf35af64ca7369a6fa6b0849643bf5",
+    "url": "404.html"
+  },
+  {
+    "revision": "1f512c5cb84862213e35cfe0d1d5af05",
+    "url": "apple-touch-icon.png"
+  },
+  {
+    "revision": "061f63e8fbf1d2fe82cd8bf4d46f0a0c",
+    "url": "browserconfig.xml"
+  },
+  {
+    "revision": "e3a673fc5552fc0cef1b",
+    "url": "css/app.384f9967.css"
+  },
+  {
+    "revision": "a08e55f0c7153dd0a57f",
+    "url": "css/chunk-22fdbae2.db95ce86.css"
+  },
+  {
+    "revision": "5dd7e86dcca05c13fa48",
+    "url": "css/chunk-63931e28.78b9cff2.css"
+  },
+  {
+    "revision": "8768b7339a788d61e6ae",
+    "url": "css/chunk-vendors.3c903ecd.css"
+  },
+  {
+    "revision": "4f4f8967d594d56b021ca4ab4977b0bf",
+    "url": "favicon-16x16.png"
+  },
+  {
+    "revision": "392f5aef65b7224663140666b6262f5e",
+    "url": "favicon-32x32.png"
+  },
+  {
+    "revision": "bb56ee978ba8ccd7a445cb51ebe4a35f",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "d3dfc1744898b708a94d155eacae3cd5",
+    "url": "firebase-messaging-sw.js"
+  },
+  {
+    "revision": "03783c5172ee1ad128c576bf88fac168",
+    "url": "fonts/fa-brands-400.03783c51.eot"
+  },
+  {
+    "revision": "7559b3774a0625e8ca6c0160f8f6cfd8",
+    "url": "fonts/fa-brands-400.7559b377.woff2"
+  },
+  {
+    "revision": "ed2b8bf117160466ba6220a8f1da54a4",
+    "url": "fonts/fa-brands-400.ed2b8bf1.ttf"
+  },
+  {
+    "revision": "fe9d62e0d16a333a20e63c3e7595f82e",
+    "url": "fonts/fa-brands-400.fe9d62e0.woff"
+  },
+  {
+    "revision": "59215032a4397507b80e5625dc323de3",
+    "url": "fonts/fa-regular-400.59215032.ttf"
+  },
+  {
+    "revision": "e07d9e40b26048d9abe2ef966cd6e263",
+    "url": "fonts/fa-regular-400.e07d9e40.woff2"
+  },
+  {
+    "revision": "e5770f9863963fb576942e25214a226d",
+    "url": "fonts/fa-regular-400.e5770f98.woff"
+  },
+  {
+    "revision": "fc9c63c8224fb341fc933641cbdd12ef",
+    "url": "fonts/fa-regular-400.fc9c63c8.eot"
+  },
+  {
+    "revision": "4bced7c4c0d61d4f988629bb8ae80b8b",
+    "url": "fonts/fa-solid-900.4bced7c4.woff"
+  },
+  {
+    "revision": "acf50f59802f20d8b45220eaae532a1c",
+    "url": "fonts/fa-solid-900.acf50f59.ttf"
+  },
+  {
+    "revision": "b5cf8ae26748570d8fb95a47f46b69e1",
+    "url": "fonts/fa-solid-900.b5cf8ae2.woff2"
+  },
+  {
+    "revision": "ef3df98419d143d9617fe163bf4edc0b",
+    "url": "fonts/fa-solid-900.ef3df984.eot"
+  },
+  {
+    "revision": "29f9630f7d87a79830d1c321e1600f2e",
+    "url": "fonts/typicons.29f9630f.ttf"
+  },
+  {
+    "revision": "95aa28e29618c068e8a53f64c87cb6a9",
+    "url": "fonts/typicons.95aa28e2.woff"
+  },
+  {
+    "revision": "a509074c88f07e40a8d2f158c4fcdda8",
+    "url": "fonts/typicons.a509074c.eot"
+  },
+  {
+    "revision": "d1f95353a09c7afd544e9ae2bc8f5af7",
+    "url": "img/fa-brands-400.d1f95353.svg"
+  },
+  {
+    "revision": "6c6a5fad8ed5136b92d0ead30b2328d3",
+    "url": "img/fa-regular-400.6c6a5fad.svg"
+  },
+  {
+    "revision": "0ffddc07ec1115efbe1833a0023a711e",
+    "url": "img/fa-solid-900.0ffddc07.svg"
+  },
+  {
+    "revision": "9b1db0de7ec18b41fb76e5c1d31564fb",
+    "url": "img/icon.9b1db0de.png"
+  },
+  {
+    "revision": "40348d7d6b7208c5d939405d829a2257",
+    "url": "img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "4e8945b09132396702c6d3d8d6663ed5",
+    "url": "img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "b9aa277fcfc34c31db6c7a7ea3469b8c",
+    "url": "img/icons/site.webmanifest"
+  },
+  {
+    "revision": "cae8fddf5efaa40d4b30d3ec57f7d979",
+    "url": "img/typicons.cae8fddf.svg"
+  },
+  {
+    "revision": "d01950f4938cfd0431c01c566f0c825b",
+    "url": "index.html"
+  },
+  {
+    "revision": "e3a673fc5552fc0cef1b",
+    "url": "js/app.da7338c1.js"
+  },
+  {
+    "revision": "a08e55f0c7153dd0a57f",
+    "url": "js/chunk-22fdbae2.4dac966d.js"
+  },
+  {
+    "revision": "5dd7e86dcca05c13fa48",
+    "url": "js/chunk-63931e28.bf1af07d.js"
+  },
+  {
+    "revision": "8768b7339a788d61e6ae",
+    "url": "js/chunk-vendors.f3312738.js"
+  },
+  {
+    "revision": "83da4801089cd908e302",
+    "url": "js/page-index.db6372a3.js"
+  },
+  {
+    "revision": "04dd4a6b374852a7830cab7d67e21f62",
+    "url": "mstile-150x150.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "b10bf91f0d794a46abdee28c7bdf69e2",
+    "url": "safari-pinned-tab.svg"
+  },
+  {
+    "revision": "3915c3909c6d7ee8b4793fa0a39a537e",
+    "url": "sitemap.xml"
+  }
+]);
